851c9fbfb178f0ba1f5d33f9fa8f9d44da24ec8d
LeeANN3 <kikilee95@gmail.com>
2026-08-02 20:33:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ async function loginStudio() {
 
         const cleanInput = input.trim();
         if (cleanInput.length < 4) {
-            alert("❌ 通行码格式不正确！请输入形如 ZSY8888 的组合。");
+            alert("❌ 通行码格式不正确！请输入形如 SLH**** 的组合。");
             location.reload();
             return;
         }
